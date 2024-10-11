90f9f68aa2db1f10d7a37c436e970696e4128990
Parv <parvruhil68@gmail.com>
2024-10-11 21:13:57 +0530

    app file updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import  { useState } from "react";
-
+import { useState } from "react";
 
 function App() {
   const [contact, setContact] = useState({
@@ -7,48 +6,47 @@ function App() {
     lName: "",
     email: ""
   });
-  function handleChange(event){
-    const {name, value} = event.target;
+
+  function handleChange(event) {
+    const { name, value } = event.target;
 
     setContact(prevValue => {
-      if(name === "fName"){
-        return {
-          fName: value,
-          lName: prevValue.lName,
-          email: prevValue.email
-        }
-      }
-      else if(name === "lName"){
-        return{
-          fName: prevValue.fName,
-          lName: value,
-          email: prevValue.email
-        }
-      }
-      else if(name === "email"){
-        return{
-          fName: prevValue.fName,
-          lName: prevValue.lName,
-          email: value,
-        }
-      }  
-    })
+      return {
+        ...prevValue,
+        [name]: value
+      };
+    });
   }
 
   return (
-    <>
-      <div className='container'>
-        <h1>Hello {contact.fName}{contact.lName}</h1>
-        <p>{contact.email}</p>
-        <form>
-          <input onChange={handleChange} value={contact.fName} name="fName" placeholder="First Name" />
-          <input onChange={handleChange} value={contact.lName} name="lName" placeholder="Last Name" />
-          <input onChange={handleChange} value={contact.email} name="email" placeholder="Email" />
-          <button>Submit</button>
-        </form>
-      </div>
-    </>
-  )
+    <div className="container">
+      <h1>
+        Hello {contact.fName} {contact.lName}
+      </h1>
+      <p>{contact.email}</p>
+      <form>
+        <input
+          onChange={handleChange}
+          name="fName"
+          value={contact.fName}
+          placeholder="First Name"
+        />
+        <input
+          onChange={handleChange}
+          name="lName"
+          value={contact.lName}
+          placeholder="Last Name"
+        />
+        <input
+          onChange={handleChange}
+          name="email"
+          value={contact.email}
+          placeholder="Email"
+        />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
 }
 
-export default App
+export default App;
